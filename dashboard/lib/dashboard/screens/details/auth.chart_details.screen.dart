9d5dc57d91429900e894b0ac604ab68b65974698
Rajan Paudel <rajan.rp50@gmail.com>
2022-08-28 16:49:43 +0545

    Auth line and histogram plot

--- dashboard/lib/dashboard/screens/details/auth.chart_details.screen.dart
+++ dashboard/lib/dashboard/screens/details/auth.chart_details.screen.dart
@@ -1,16 +1,16 @@
 // class ChartDetailsScreen extends StatelessWidget {
 
+import 'dart:async';
+import 'dart:convert';
 import 'dart:developer';
 
 import 'package:dashboard/dashboard/widgets/index.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:loading_indicator/loading_indicator.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
-import 'package:syncfusion_flutter_charts/sparkcharts.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 import '../../model/auth_log_model.dart';
-import '../../services/auth_log_services.dart';
 
 class AuthChartDetailsScreen extends StatefulWidget {
   final String fileName;
@@ -22,16 +22,58 @@ class AuthChartDetailsScreen extends StatefulWidget {
 }
 
 class _AuthChartDetailsScreenState extends State<AuthChartDetailsScreen> {
-  late List<AuthLogModel> authData;
+  List<AuthLogModel> chartData = [];
+  List<AuthLogModel> livePlot = [];
+  ChartSeriesController? _chartSeriesController;
   late TooltipBehavior _tooltipBehavior;
-  List<AuthLogModel> barData = [];
-  int click = 0;
+  Timer? timer;
+  int data = 0;
+  void _updateDataSource(Timer timer) {
+    // if(isCardView != null){
+    log(chartData.length.toString());
+    livePlot = chartData.sublist(data + 40, (data + 40) + 40);
+
+    // livePlot.add(chartData[i]);
+    log("length of liveplot dtata${livePlot.length}");
+    log("i ko lethgth$data");
+    if (livePlot.length == 40) {
+      // livePlot.removeAt(0);
+      _chartSeriesController?.updateDataSource(
+        addedDataIndexes: <int>[livePlot.length + 1],
+        // removedDataIndexes: <int>[0],
+      );
+    } else {
+      _chartSeriesController?.updateDataSource(
+        addedDataIndexes: <int>[livePlot.length - 1],
+      );
+    }
+    data++;
+    // }
+  }
+
+  Future loadAuthLog() async {
+    final String jsonString =
+        await getJsonFromAssets(filename: widget.fileName);
+    final dynamic jsonResponse = json.decode(jsonString);
+    for (Map<String, dynamic> i in jsonResponse) {
+      chartData.add(AuthLogModel.fromJson(i));
+    }
+  }
+
+  Future<String> getJsonFromAssets({
+    required String filename,
+  }) async {
+    return await rootBundle.loadString(filename);
+  }
 
   @override
   void initState() {
-    // Timer.periodic(const Duration(milliseconds: 1), (timer) {
-    //   _updateDataSource(timer, i++);
-    // });
+    log(widget.fileName);
+    loadAuthLog();
+    timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      _updateDataSource(timer);
+      setState(() {});
+    });
     _tooltipBehavior = TooltipBehavior(
         enable: true,
         builder: (dynamic data, dynamic point, dynamic series, int pointIndex,
@@ -107,46 +149,62 @@ class _AuthChartDetailsScreenState extends State<AuthChartDetailsScreen> {
 
                     // color: Colors.blue,
                     child: FutureBuilder(
-                        future: readJsonAuthLog(filename: widget.fileName),
-                        builder: (context, data) {
-                          if (data.hasError) {
-                            return Center(
-                              child: Text("Error::::${data.error}"),
-                            );
-                          } else if (data.hasData) {
-                            authData = data.data as List<AuthLogModel>;
-
-                            log(authData.length.toString());
-                            if (kDebugMode) {
-                              print(authData[0].distance);
-                            }
-
-                            return Padding(
-                              padding: const EdgeInsets.all(50.0),
-                              child: SfSparkLineChart.custom(
-                                color: Colors.black,
-                                marker: const SparkChartMarker(
-                                    displayMode:
-                                        SparkChartMarkerDisplayMode.all,
-                                    shape: SparkChartMarkerShape.circle),
-                                trackball: const SparkChartTrackball(
-                                  activationMode: SparkChartActivationMode.tap,
-                                  shouldAlwaysShow: true,
-                                ),
-                                // axisLineDashArray: const <double>[0, 0],
-                                axisCrossesAt: 0,
-                                axisLineColor: Colors.black,
-
-                                // plotBand: const SparkChartPlotBand(
-                                //     start: 7, end: 14, color: Colors.yellow),
-
-                                xValueMapper: (int index) => authData[index].ip,
-                                // Binding the y values
-                                yValueMapper: (int index) =>
-                                    authData[index].modZscore!,
-                                // Assigning the number of data.
-                                dataCount: 50,
-                              ),
+                        future: getJsonFromAssets(filename: widget.fileName),
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData) {
+                            return SfCartesianChart(
+                              title: ChartTitle(text: "Live Plot"),
+                              enableAxisAnimation: true,
+                              // tooltipBehavior: _tooltipBehavior,
+                              enableMultiSelection: true,
+                              plotAreaBorderWidth: 0,
+                              primaryXAxis: CategoryAxis(
+                                  labelRotation: 90,
+                                  majorGridLines:
+                                      const MajorGridLines(width: 0),
+                                  isVisible: true),
+                              primaryYAxis: NumericAxis(
+                                  plotBands: <PlotBand>[
+                                    PlotBand(
+                                        isVisible: true,
+                                        start: 3,
+                                        end: 3.1,
+                                        color: Colors.red),
+                                  ],
+                                  labelIntersectAction:
+                                      AxisLabelIntersectAction.rotate45,
+                                  axisLine: const AxisLine(width: 0),
+                                  majorTickLines:
+                                      const MajorTickLines(size: 10)),
+                              series: <LineSeries<AuthLogModel, String>>[
+                                LineSeries<AuthLogModel, String>(
+                                  markerSettings: const MarkerSettings(
+                                    isVisible: true,
+                                  ),
+                                  // dataLabelSettings: DataLabelSettings(
+                                  //     isVisible: true,
+                                  //     builder: (dynamic data, dynamic point, dynamic series,
+                                  //         int pointIndex, int seriesIndex) {
+                                  //       return SizedBox(
+                                  //           height: 30,
+                                  //           width: 30,
+                                  //           child: data.modZscore > 5
+                                  //               ? const Text("threat")
+                                  //               : Container());
+                                  //     }),
+                                  onRendererCreated:
+                                      (ChartSeriesController controller) {
+                                    _chartSeriesController = controller;
+                                  },
+                                  dataSource: livePlot,
+                                  color: const Color.fromRGBO(192, 108, 132, 1),
+                                  xValueMapper: (AuthLogModel auth, _) =>
+                                      auth.time,
+                                  yValueMapper: (AuthLogModel auth, _) =>
+                                      auth.modZscore,
+                                  // animationDuration: 0,
+                                )
+                              ],
                             );
                           } else {
                             return const Center(
@@ -198,31 +256,63 @@ class _AuthChartDetailsScreenState extends State<AuthChartDetailsScreen> {
                         ),
                       ],
                       borderRadius: BorderRadius.circular(20)),
-                  child: Stack(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: SfCartesianChart(
-                          series: <ChartSeries>[
-                            ColumnSeries<AuthLogModel, String>(
-                              dataSource: barData,
-                              xValueMapper: (AuthLogModel m, index) => m.time,
-                              yValueMapper: (AuthLogModel m, index) => m.label,
-                            )
-                          ],
-                          primaryXAxis: CategoryAxis(
-                              labelRotation: 45,
-                              title: AxisTitle(text: "Time")),
-                          primaryYAxis:
-                              NumericAxis(title: AxisTitle(text: "Label")),
+                  child: FutureBuilder(
+                      future: getJsonFromAssets(filename: widget.fileName),
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData) {
+                          return SfCartesianChart(
+                            title: ChartTitle(text: "Histogram Chart"),
+                            legend: Legend(isVisible: true),
+                            series: <ChartSeries>[
+                              HistogramSeries<AuthLogModel, num>(
+                                  dataLabelSettings: const DataLabelSettings(
+                                      angle: 45, isVisible: true),
+                                  name: "Histogram",
+                                  dataSource: chartData,
+                                  yValueMapper: (AuthLogModel auth, _) =>
+                                      auth.label,
+                                  binInterval: 1,
+                                  showNormalDistributionCurve: true,
+                                  spacing: 0,
+                                  // animationDelay: ,
+                                  enableTooltip: true,
+                                  curveColor:
+                                      const Color.fromRGBO(192, 108, 132, 1),
+                                  borderWidth: 3),
+                            ],
+                          );
+                        } else {
+                          return const Center(
+                            child: SizedBox(
+                              height: 100,
+                              width: 100,
+                              child: LoadingIndicator(
+                                  indicatorType:
+                                      Indicator.lineScalePulseOutRapid,
 
-                          tooltipBehavior: _tooltipBehavior,
-                          // isTransposed: false,
-                          // enableAxisAnimation: true,
-                        ),
-                      ),
-                    ],
-                  ),
+                                  /// Required, The loading type of the widget
+                                  colors: [
+                                    Colors.black,
+                                    Colors.grey,
+                                    Colors.red,
+                                    Colors.green
+                                  ],
+
+                                  /// Optional, The color collections
+                                  strokeWidth: 2,
+
+                                  /// Optional, The stroke of the line, only applicable to widget which contains line
+                                  backgroundColor: Colors.transparent,
+
+                                  /// Optional, Background of the widget
+                                  pathBackgroundColor: Colors.black
+
+                                  /// Optional, the stroke backgroundColor
+                                  ),
+                            ),
+                          );
+                        }
+                      }),
                 ),
               ],
             ),
