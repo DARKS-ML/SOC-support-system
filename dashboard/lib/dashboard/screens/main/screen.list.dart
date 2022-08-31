6ee6ff0a9d192c59dd7c8ce9fba1a6789f5d5ea6
Rajan Paudel <rajan.rp50@gmail.com>
2022-08-31 13:32:56 +0545

    Auth anomaly csv with filter

--- dashboard/lib/dashboard/screens/main/screen.list.dart
+++ dashboard/lib/dashboard/screens/main/screen.list.dart
@@ -7,26 +7,26 @@ import '../previousresult.screen.dart';
 
 class DashboardScreenList {
   static final sidebarScreens = [
-    ListDataSetScreen(),
-    OverViewScreen(),
-    PreviousPredictResultScreen(),
+    const ListDataSetScreen(),
+    const PreviousPredictResultScreen(),
+    const OverViewScreen(),
     const Logout(),
   ];
   static final sidebarScreensname = [
     SideBarItem(
       text: "Dashboard",
       icon: Icons.dashboard,
-      tooltipText: "Others page",
-    ),
-    SideBarItem(
-      text: "Overview",
-      icon: Icons.other_houses_sharp,
-      tooltipText: "Overview page",
+      tooltipText: "DashBoard",
     ),
     SideBarItem(
       text: "Previous Predict",
       icon: Icons.skip_previous,
       tooltipText: "Previous Result Page",
+    ),
+    SideBarItem(
+      text: "Overview",
+      icon: Icons.all_inclusive,
+      tooltipText: "Auth Anomoly CSV",
     ),
     SideBarItem(
       text: "Logout",
@@ -50,7 +50,7 @@ class Logout extends StatelessWidget {
             style: TextStyle(fontWeight: FontWeight.bold),
           ),
           const SizedBox(height: 30),
-          GestureDetector(
+          InkWell(
             onTap: () => Navigator.pushReplacementNamed(context, '/login'),
             child: Container(
               padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 6),
